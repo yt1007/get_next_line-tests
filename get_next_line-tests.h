38b45e0114886689bc5a11be8df257decf6a0029
Yea Ling Tay <yetay@u82z03s02.42kl.edu.my>
2023-07-17 08:14:12 +0800

    Added string.h include in header

--- get_next_line-tests.h
+++ get_next_line-tests.h
@@ -6,13 +6,16 @@
 /*   By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 18:54:08 by yetay             #+#    #+#             */
-/*   Updated: 2023/07/14 12:17:23 by yetay            ###   ########.fr       */
+/*   Updated: 2023/07/17 07:42:45 by yetay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_TESTS_H
 # define GET_NEXT_LINE_TESTS_H
 
+# include <string.h>
+
 void	err_putstr(char *s);
 void	err_putnbr(int n);
+
 #endif
