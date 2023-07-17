284c82dd25256125dd7844111ed889e65d10ba91
Yea Ling Tay <yetay@u82z05s06.42kl.edu.my>
2023-07-18 07:56:53 +0800

    Added file name builder function to increase readability

--- tests/bonus/test.c
+++ tests/bonus/test.c
@@ -6,12 +6,23 @@
 /*   By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 19:21:18 by yetay             #+#    #+#             */
-/*   Updated: 2023/07/18 07:45:04 by yetay            ###   ########.fr       */
+/*   Updated: 2023/07/18 07:56:10 by yetay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 #include "get_next_line-tests.h"
+
+static char	*build_fn(char *prefix, int i, char *suffix)
+{
+	char	*fn;
+
+	fn = calloc(strlen(prefix) + 1 + strlen(suffix) + 1, 1);
+	strcat(fn, prefix);
+	fn[strlen(fn)] = i + '0';
+	strcat(fn, suffix);
+	return (fn);
+}
 
 static int	gnl_is_diff(int i, char *f1, char *f2)
 {
@@ -40,10 +51,7 @@ static int	gnl_is_diff(int i, char *f1, char *f2)
 	if (! diff)
 	{
 		unlink(f2);
-		cmd = calloc(11, 1);
-		strcat(cmd, "diff_");
-		cmd[strlen(cmd)] = i + '0';
-		strcat(cmd, ".out");
+		cmd = build_fn("diff_", i, ".out");
 		unlink(cmd);
 		free(cmd);
 	}
@@ -63,16 +71,10 @@ int	main(void)
 	i = 0;
 	while (++i <= 9)
 	{
-		fn = calloc(strlen("tests/bonus/input_") + 1 + strlen(".txt") + 1, 1);
-		strcat(fn, "tests/bonus/input_");
-		fn[strlen(fn)] = i + '0';
-		strcat(fn, ".txt");
+		fn = build_fn("tests/bonus/input_", i, ".txt");
 		fd[i - 1] = open(fn, O_RDONLY);
 		free(fn);
-		fn = calloc(strlen("gnl_bonus_") + 1 + strlen(".txt") + 1, 1);
-		strcat(fn, "gnl_bonus_");
-		fn[strlen(fn)] = i + '0';
-		strcat(fn, ".txt");
+		fn = build_fn("gnl_bonus_", i, ".txt");
 		ofd[i - 1] = open(fn, O_WRONLY | O_CREAT, 0644);
 		gnl = get_next_line(fd[i - 1]);
 		if (gnl)
@@ -84,10 +86,7 @@ int	main(void)
 			write(ofd[i - 1], gnl, 0);
 		close(ofd[i - 1]);
 		close(fd[i - 1]);
-		efn = calloc(strlen("tests/bonus/output_") + 1 + strlen(".txt") + 1, 1);
-		strcat(efn, "tests/bonus/output_");
-		efn[strlen(efn)] = i + '0';
-		strcat(efn, ".txt");
+		efn = build_fn("tests/bonus/output_", i, ".txt");
 		diff = gnl_is_diff(i, efn, fn);
 		free(fn);
 		free(efn);
