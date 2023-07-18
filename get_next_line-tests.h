b88c0b8e4fc26cf3718f640671607da3ad138753
Yea Ling Tay <yetay@u82z05s06.42kl.edu.my>
2023-07-18 09:25:38 +0800

    included stdlib.h and fcntl.h

--- get_next_line-tests.h
+++ get_next_line-tests.h
@@ -6,13 +6,15 @@
 /*   By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 18:54:08 by yetay             #+#    #+#             */
-/*   Updated: 2023/07/17 07:42:45 by yetay            ###   ########.fr       */
+/*   Updated: 2023/07/18 09:22:13 by yetay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_TESTS_H
 # define GET_NEXT_LINE_TESTS_H
 
+# include <fcntl.h>
+# include <stdlib.h>
 # include <string.h>
 
 void	err_putstr(char *s);
