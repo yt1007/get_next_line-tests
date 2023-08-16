0338d51567fde8f7c0296816543243fe39298c87
Yea Ling Tay <yetay@u90z04s06.42kl.edu.my>
2023-08-16 08:13:37 +0800

    Used limits.h to handle max file descriptors

--- tests/errors/test.c
+++ tests/errors/test.c
@@ -6,11 +6,11 @@
 /*   By: yetay <yetay@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 18:54:08 by yetay             #+#    #+#             */
-/*   Updated: 2023/08/16 07:56:25 by yetay            ###   ########.fr       */
+/*   Updated: 2023/08/16 08:13:09 by yetay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include <limits.h>
 #include "get_next_line.h"
 #include "get_next_line-tests.h"
 
@@ -36,22 +36,20 @@ static int	run_fd_test(int fd)
 
 /* This test passes arbitrary file descriptor to the get_next_line function   */
 /* on which it is not possible to read. The function must return NULL.        */
-int	main(int argc, char **argv)
+int	main(void)
 {
 	int		errors;
 
-	if (argc != 2)
-		return (0);
 	errors = 0;
-	if (atoi(argv[1]) > -1 && run_fd_test(-1))
+	if (OPEN_MAX > -1 && run_fd_test(-1))
 		errors++;
-	if (atoi(argv[1]) > 42 && run_fd_test(42))
+	if (OPEN_MAX > 42 && run_fd_test(42))
 		errors++;
-	if (atoi(argv[1]) > 1024 && run_fd_test(1024))
+	if (OPEN_MAX > 1024 && run_fd_test(1024))
 		errors++;
-	if (atoi(argv[1]) > 4096 && run_fd_test(4096))
+	if (OPEN_MAX > 4096 && run_fd_test(4096))
 		errors++;
-	if (atoi(argv[1]) > 124816 && run_fd_test(124816))
+	if (OPEN_MAX > 124816 && run_fd_test(124816))
 		errors++;
 	if (errors)
 		return (1);
